4e445fc95b06cbdee318c02f2018e1e35bcc0072
ErickBGomez <90425287+ErickBGomez@users.noreply.github.com>
2024-01-06 22:38:11 -0600

    Add sidebar basic styling

--- src/sidebar.js
+++ src/sidebar.js
@@ -1,6 +1,12 @@
 export default function renderSidebar() {
   const sidebar = document.createElement("div");
-  sidebar.textContent = "To-do app";
+  sidebar.id = "sidebar";
+
+  const appTitle = document.createElement("div");
+  appTitle.id = "app-title";
+  appTitle.textContent = "To-do app";
+
+  sidebar.appendChild(appTitle);
 
   document.body.appendChild(sidebar);
 }
