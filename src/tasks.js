f7f9f4c25012bea3d607950216cdd92f646394ea
ErickBGomez <90425287+ErickBGomez@users.noreply.github.com>
2024-01-10 09:12:08 -0600

    Add default values to create tasks

--- src/tasks.js
+++ src/tasks.js
@@ -1,7 +1,7 @@
 export default function createTask() {
-  const title = prompt("Title");
-  const description = prompt("Description");
-  const date = prompt("Date");
+  const title = prompt("Title", "Task title");
+  const description = prompt("Description", "Description of the task");
+  const date = prompt("Date", "Jan 13, 2024");
   const priority = Number(prompt("priority (1,2,3)"));
 
   return { title, description, date, priority };
