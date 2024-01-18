b15dbaaa58f1bd704e6576cea12d96caa74a7c1f
ErickBGomez <90425287+ErickBGomez@users.noreply.github.com>
2024-01-17 21:48:51 -0600

    Create new task dialog box

--- src/design/dialogs.js
+++ src/design/dialogs.js
@@ -20,6 +20,7 @@ const dialogs = (() => {
     return buttons;
   };
 
+  // New list:
   const showNewListDialog = () => {
     const dialog = document.createElement("dialog");
     dialog.id = "new-list";
@@ -31,9 +32,10 @@ const dialogs = (() => {
     titleIconInput.className = "title-icon-input";
 
     const titleInput = document.createElement("input");
-    titleInput.id = "list-title-input";
     titleInput.type = "text";
-    titleInput.placeholder = "New list title";
+    titleInput.id = "list-title-input";
+    titleInput.name = "list-title-input";
+    titleInput.placeholder = "List title";
 
     titleContainer.appendChild(titleIconInput);
     titleContainer.appendChild(titleInput);
@@ -49,6 +51,64 @@ const dialogs = (() => {
     dialog.showModal();
   };
 
+  const addCreateListEvent = (dialogContainer) => {
+    const createButton = dialogContainer.querySelector("button.primary");
+    const titleInput = dialogContainer.querySelector("input#list-title-input");
+
+    createButton.addEventListener("click", () => {
+      lists.createList(titleInput.value);
+      sidebar.refreshUserSidebarLists();
+    });
+  };
+
+  // New task:
+  const showNewTaskDialog = () => {
+    const dialog = document.createElement("dialog");
+    dialog.id = "new-task";
+
+    const titleInput = document.createElement("input");
+    titleInput.id = "task-title-input";
+    titleInput.type = "text";
+    titleInput.placeholder = "Task title";
+
+    const descriptionInput = document.createElement("textarea");
+    descriptionInput.id = "task-description-input";
+    descriptionInput.name = "task-description-input";
+    descriptionInput.rows = "4";
+    descriptionInput.placeholder = "Task description";
+
+    const taskOptions = document.createElement("div");
+    taskOptions.className = "task-options";
+
+    const dateInput = document.createElement("div");
+    dateInput.id = "task-date-input";
+    dateInput.textContent = "Date";
+
+    const priorityInput = document.createElement("div");
+    priorityInput.id = "task-priority-input";
+    priorityInput.textContent = "Priority";
+
+    const listInput = document.createElement("div");
+    listInput.id = "task-list-input";
+    listInput.textContent = "List";
+
+    taskOptions.appendChild(dateInput);
+    taskOptions.appendChild(priorityInput);
+    taskOptions.appendChild(listInput);
+
+    dialog.appendChild(titleInput);
+    dialog.appendChild(descriptionInput);
+    dialog.appendChild(taskOptions);
+    dialog.appendChild(appendDialogButtons("Add task"));
+
+    document.body.appendChild(dialog);
+
+    addCloseDialogEvent(dialog);
+
+    dialog.showModal();
+  };
+
+  // All dialogs:
   const closeDialogs = () => {
     const dialog = document.querySelector("dialog[open]");
     dialog.close();
@@ -61,17 +121,7 @@ const dialogs = (() => {
     buttons.forEach((button) => button.addEventListener("click", closeDialogs));
   };
 
-  const addCreateListEvent = (dialogContainer) => {
-    const createButton = dialogContainer.querySelector("button.primary");
-    const titleInput = dialogContainer.querySelector("input#list-title-input");
-
-    createButton.addEventListener("click", () => {
-      lists.createList(titleInput.value);
-      sidebar.refreshUserSidebarLists();
-    });
-  };
-
-  return { showNewListDialog };
+  return { showNewListDialog, showNewTaskDialog };
 })();
 
 export { dialogs };
