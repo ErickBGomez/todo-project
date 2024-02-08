aac50d0aec82b393e8da8c84465d053e8aba8522
ErickBGomez <90425287+ErickBGomez@users.noreply.github.com>
2024-02-07 18:58:47 -0600

    Remove home default lists from sidebar

--- src/design/sidebar.js
+++ src/design/sidebar.js
@@ -1,6 +1,3 @@
-import allTasksSvg from "../img/svg/all-tasks.svg";
-import todaySvg from "../img/svg/today.svg";
-import thisWeekSvg from "../img/svg/this-week.svg";
 import defaultListSvg from "../img/svg/default-list.svg";
 import { loadListContent } from "./page";
 import lists from "../lists-and-tasks.js";
@@ -21,8 +18,9 @@ function createListSidebar(listLabel, listIcon = defaultListSvg) {
   list.className = "list-sidebar-element";
   list.dataset.listName = listLabel;
 
-  const icon = new Image();
-  icon.src = listIcon;
+  const icon = document.createElement("div");
+  icon.className = "lsit-sidebar-element-icon";
+  icon.innerHTML = listIcon;
 
   const label = document.createElement("div");
   label.className = "list-sidebar-element-label";
@@ -32,27 +30,6 @@ function createListSidebar(listLabel, listIcon = defaultListSvg) {
   list.appendChild(label);
 
   return list;
-}
-
-function createHomeListsContainer() {
-  const listsContainer = document.createElement("div");
-  listsContainer.className = "sidebar-lists-container";
-
-  const title = document.createElement("div");
-  title.className = "title";
-  title.textContent = "Home";
-
-  const listsGroup = document.createElement("div");
-  listsGroup.className = "lists-group";
-
-  listsGroup.appendChild(createListSidebar("All Tasks", allTasksSvg));
-  listsGroup.appendChild(createListSidebar("Today", todaySvg));
-  listsGroup.appendChild(createListSidebar("This week", thisWeekSvg));
-
-  listsContainer.appendChild(title);
-  listsContainer.appendChild(listsGroup);
-
-  return listsContainer;
 }
 
 export function refreshUserSidebarLists() {
@@ -138,7 +115,6 @@ export default function renderSidebar() {
   sidebar.id = "sidebar";
 
   sidebar.appendChild(createAppTitle());
-  sidebar.appendChild(createHomeListsContainer());
   sidebar.appendChild(createUserListsContainer());
   sidebar.appendChild(createLowerSidebar());
 
